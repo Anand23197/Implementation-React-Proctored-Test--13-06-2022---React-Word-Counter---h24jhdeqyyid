11847a5e55b4525874a09be61541fdeadd8b9de1
ANAND VISHWAKARMA <anand23197@gmail.com>
2022-06-13 21:45:59 +0530

    thi

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
 const [text, setText] = useState('');
-// const[wordCount, setWordCount] = useState(0);
-// const[charCount, setCharCount] = useState(0);
 const[increase, setIncrease] = useState(16);
 const[charLimit, setCharLimit] = useState(50);
 
@@ -26,9 +24,9 @@ setIncrease(event.target.value);
       <h2>Font size picker</h2>
       <input type="range" id='fontSize-input' min="16" max="32" onChange={setChange}/>
       <h3>word Limit input</h3>
-      <input type="number" id='char-limit-input' max={charLimit} onChange={setCharacterLimit}/><br/>
+      <input type="number" id='char-limit-input' min="0" onChange={setCharacterLimit}/><br/>
       <textarea rows="4" cols="40" onChange={handleOnChange} style={{fontSize:`${increase}px`}} maxLength={charLimit}/>
-      <div id='word-counter'>Total number of words written: {text.split(" ").length-1} </div>
+      <div id='word-counter'>Total number of words written: {text.split(" ").length} </div>
       <div id='char-counter'>Total number of characters used: {text.length}</div>
     </div>
   )
